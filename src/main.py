81328def16e319757c51de66da38fcc485b3b1c2
Vladama <mcartron01@gmail.com>
2021-03-15 12:47:26 +0100

    V1

--- src/main.py
+++ src/main.py
@@ -16,7 +16,7 @@ def get_parent_dir(n=1):
     return cpath
 
 def create_model(train_weight_final,anchors_path,yolo_classname, vpath):
-    print("A")
+
     score = 0.25
     num_gpu = 1
     yolo = y.YOLO(
@@ -29,32 +29,30 @@ def create_model(train_weight_final,anchors_path,yolo_classname, vpath):
             "model_image_size": (416, 416),
         }
     )
-    print("B")
+
     out_df = pd.DataFrame(
         columns=[
-            "image",
-            "image_path",
+            "frame_ID",
             "xmin",
             "ymin",
             "xmax",
             "ymax",
             "label",
             "confidence",
-            "x_size",
-            "y_size",
+            "position",
         ]
     )
-    print("C")
+
     # labels to draw on images
     class_file = open(yolo_classname, "r")
-    print("D")
+
     input_labels = [line.rstrip("\n") for line in class_file.readlines()]
     print("Found {} input labels: {} ...".format(len(input_labels), input_labels))
 
-    y.detect_video(yolo, vpath, output_path="testPA8")
-    print("D")
+    y.detect_video(yolo, vpath, output_path="testPA8_detect2.mp4")
+
     yolo.close_session()
-    print("E")
+
 
 def videotoclips():
     clips = []
@@ -77,24 +75,12 @@ def main():
     yolo_log_dir = os.path.join(Yolo_Model_Folder, "logs")
     train_weight_final = os.path.join(yolo_log_dir, "trained_weights_final.h5")
 
-
     ressources = os.path.join(get_parent_dir(0), "res")
     video = os.path.join(ressources, "video")
     videotest = os.path.join(video, "video.mp4")
 
     create_model(train_weight_final, anchors_path, YOLO_classname, videotest)
 
-
-""" Data_Folder = os.path.join(get_parent_dir(0), "data")
-    MXNet_Train_Folder = os.path.join(Data_Folder, "MXNetframes")
-    Yolo_Train_Folder = os.path.join(Data_Folder, "YoloTrainingImages")
-    YOLO_filename = os.path.join(Yolo_Train_Folder, "data_train.txt")
-    train_weight_stage = os.path.join(yolo_log_dir, "trained_weights_stage_1.h5")
-    yolo_train_weights = os.path.join(keras_path, "yolo.h5")
-    image_test_folder = os.path.join(Data_Folder, "Test")
-    detection_results_folder = os.path.join(Data_Folder, "Test_Results")
-    detection_results_file = os.path.join(detection_results_folder, "Yolo_Detection_Results.csv")
-    yolo_test_weights = os.path.join(Yolo_Model_Folder, "logs", "trained_weights_final.h5")"""
 
 if __name__ == '__main__':
     main()
